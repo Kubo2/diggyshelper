3b54871b8097bf4af73bbfd918ba370b96250517
Kubo2 <kelerest123@gmail.com>
2014-03-26 23:02:28 +0100

    Pridané nejaké misc funkcie
     - Môžu byť v budúcnosti odstránené, pokiaľ sa ukážu ako nevyužiteľné.

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,30 @@
-<?php
-
-
-
-?>
+<?php
+
+/**
+ * Whether at least one field of array is empty.
+ *
+ * @param array
+ * @return bool true if at least one array field is empty, otherwise false
+ */
+
+function emptyArray(array $array) {
+	foreach($array as $field) {
+		if(empty($field)){
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * Which fields in array are empty.
+ *
+ * @param array
+ * @return array with keys from arg1 and field values true/false (empty/not empty)
+ */
+function emptyArrayFields(array $array) {
+	$result = [];
+	foreach($array as $index => $field) {
+		$result[$index] = empty($field);
+	}
+}
