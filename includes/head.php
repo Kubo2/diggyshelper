c065bd9bc344699b7d889582f091cd20c2983901
Kubo2 <kelerest123@gmail.com>
2014-03-31 23:16:10 +0200

    Added temporary universal title.

--- includes/head.php
+++ includes/head.php
@@ -4,7 +4,8 @@
 $absUrl = rtrim(dirname($_SERVER["PHP_SELF"]), '/');
 
 ?>
-<title>Diggy's Helper - Prvé česko-slovenksé fórum</title>
+<!-- Universal title. Only temporarily. -->
+<title><?php echo !empty($titleConst) ? $titleConst : "Diggy's Helper - Prvé česko-slovenksé fórum" ?></title>
 <link rel='stylesheet' href='<?php echo $absUrl; ?>/css/style.css'>
 <link href="<?php echo $absUrl; ?>/favicon.png" rel="icon" type="image/png">
 <!--script type="text/javascript" src="<?php echo $absUrl; ?>/universal.js"></script-->
