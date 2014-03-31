6f335bdbe8d19edd367f3aaab4173a34efb96aea
Kubo2 <kelerest123@gmail.com>
2014-03-31 23:40:09 +0200

    Upravený formulár.
     * Vzhľad
     * Kód
     * Indentácia kódu (?!)
     * Odkaz na Môj profil
     * Odstránený zbytočný session_start()
     * <font color=...> -> <span style=...>

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,21 @@
 <div id="header">
-	<?php @session_start() ?>
 	<div id="loginprovizor">
-<?php if (!isset($_SESSION['uid'])) { ?>
-<form action='login.php' method='post'>
-	<input class='input' type='text' name='username' placeholder='Nickname'>&nbsp;
-	<input class='input' type='password' name='password' placeholder='Heslo'>&nbsp;
-	<input class='input' type='checkbox' name='remember'> Neodhlasovať ma&nbsp;
-	<input type='submit' name='submit' class='input_button' value='Prihlásiť sa' >&nbsp;
-	<a class='button_logout' href='#' >Zabudli ste heslo?</a>&nbsp;
-	<a class='button_register' href='register.php'>Registrovať sa</a>
-</form>
-<?php } else { ?>
-Prihlásený používateľ: 
-<font color='#106CB5'><?php echo($_SESSION['username']) ?></font> 
-&rsaquo; <a class='button' href='#'>Môj profil</a> 
-<a class='button_register' href='#'>Žiadosti o priateľstvo (0)</a> 
-<a class='button_logout' href='logout.php'>Odhlásiť sa</a>
-<?php } ?>
-</div>
+		<?php 
+		if(!isset($_SESSION['uid'])) { // TODO: restructure session array
+		?>
+		<form action='login.php' method='post'>
+			<input class='input' type='text' name='username' placeholder='Nickname'><br>
+			<input class='input' type='password' name='password' placeholder='Heslo'>&nbsp;
+			<label><input class='input' type='checkbox' name='remember'>&nbsp;Neodhlasovať ma</label><br>
+			<input type='submit' name='submit' class='input_button' value='Prihlásiť sa'>&nbsp;
+			<!--a class='button_logout' href='#' >Zabudli ste heslo?</a>&nbsp;--><!-- <<TODO -->
+			<a class='button_register' href='register.php'>Registrovať sa</a>
+		</form>
+		<?php } else { ?>
+		Prihlásený používateľ: <span style="color: #106cb5"><?php echo($_SESSION['username']) ?></span>&rsaquo;
+		<a class='button' href='./#member=<?php echo($_SESSION['uid']) ?>'>Môj profil</a>
+		<!--a class='button_register' href='#'>Žiadosti o priateľstvo (0)</a-->
+		<a class='button_logout' href='logout.php'>Odhlásiť sa</a>
+		<?php } ?>
+	</div>
 </div>
