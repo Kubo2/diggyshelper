ba7242cdf04d7b914d0d492bd2ee16994be53947
Kubo2 <kelerest123@gmail.com>
2014-04-05 17:06:44 +0200

    Fixed text centering

--- index.php
+++ index.php
@@ -15,15 +15,17 @@ session_start();
 		include 'includes/submenu.php'; 
 	?>
 	<div id="pages">
-		<div style="width: auto; margin: 0 auto;">
+		<div style="width: 100%; text-align: center;">
 			<h3>Vítam ťa na stránke Diggy's Helper</h3>
 			<img src="images/bg/logo-DA.png">
-		</div><br>
+		</div>
+		<P>
 		Kto povedal, že všetky mýtické poklady už boli objavené? Prekop sa s Diggym k tajomstvám ukrytým pod zemou. Si pripravený na dobrodružstvo? Zbožňuješ staroveké civilizácie a ich záhady? Chcel by si objavovať dávno stratené chrámy a zabudnuté kobky? Tak potom si ten pravý. Tajomstvá ukryté tisícky rokov pod zemou legendárnych území po celom svete čakajú len na tvoje šikovné ruky a myseľ objaviteľa. Pridaj sa k Profesorovi, Linde a robotovi Rustymu, postav kemp a vydaj sa na dobrodružstvo tvojho života.<br><br>
-		<center>
+		</P>
+		<div style="width: 100%; text-align: center;">
 			<a href="http://portal.pixelfederation.com/diggysadventure/?lang=SK" target="blank" class="playgamepp">Hraj na Pixel Portáli</a>
 			<a href="https://apps.facebook.com/diggysadventure/" target="blank" class="playgamefb">Hraj na Facebooku</a>
-		</center><br><br>
+		</div>
 		<b>O firme PIXEL FEDERATION:</b><br>
 		Firma <a class="memberusers" href="http://www.pixelfederation.com/?lang=sk" target="blank">PIXEL FEDERATION</a> bola založená v roku 2007 štyrmi nadšencami z herného priemyslu a venuje sa výhradne tvorbe online webových a mobilných hier, jednou z nich je aj spomínaný Diggy's Adventure. Inovatívne myslenie či entuziazmus v tíme dopĺňajú skúsenosti z vlastných herných projektov. Tá správna kombinácia ľudí s rôznym backgroundom nám umožňuje zdolávať výzvy, ktoré dnešný game development ponúka. Nájdete nás v hlavnom meste Slovenska, v Bratislave.
 	</div>
