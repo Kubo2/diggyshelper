9f24d29ec267370da93c0874e63a3422b0c3d4b7
Kubo2 <kelerest123@gmail.com>
2014-04-27 00:38:20 +0200

    Pridaná aktuálna verzia diggyshelper.js za otáznik
    Kvôli cachovaniu

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,5 @@
 	<div id="footer_right">	
 
 	</div>
-	<script type="text/javascript" src="<?php echo !empty($absUrl) ? $absUrl : '.'; ?>/diggyshelper.js"></script>
+	<script type="text/javascript" src="<?php echo !empty($absUrl) ? $absUrl : '.'; ?>/diggyshelper.js?1.0.8"></script>
 </div>
