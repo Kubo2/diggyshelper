2d08e33ea1d87d9c0c596c5ad31735629930baf6
Kubo2 <kelerest123@gmail.com>
2014-05-04 01:27:53 +0200

    Rewritten posting to existing threads.
    Prepísané pridávnie príspevkov do vlákien.
    Trocha pozmenená štruktúra databáze.

--- post_reply.php
+++ post_reply.php
@@ -2,12 +2,31 @@
 
 session_start();
 
-if(!isset($_SESSION["uid"]) || !intval(!empty($_GET["cid"]) ? $_GET["cid"] : 0)) {
+if( !isset($_SESSION["uid"]) || (!@intval($_GET["cid"]) || !@intval($_GET["tid"])) && !isset($_GET["flash"]) ) { // intentionally @
 	header("Location: http://$_SERVER[SERVER_NAME]" . dirname($_SERVER["PHP_SELF"]) . "/index.php", true, 302);
 	exit;
 }
 
 header("Content-Type: text/html; charset=utf-8", true, 200);
+
+/**
+ * Flash messages.
+ */
+$aMasTo = " Nezaškodí niekoľko minúť počkať. <noscript>Skúste sa vrátiť v histórii a opäť odoslať príspevok.</noscript><script>document.write(\"<a href='javascript:void(history.go(-1))'>Vráťte sa</a> (bez ujmy na príspevku) a skúste ho znova odoslať.\")</script>";
+$messages = [
+	"!db" => "Nebolo možné nadviazať komunikáciu s našou databázou.",
+	"!id" => "Nastala chyba. Neodoslali ste údaj o téme, do ktorej chcete prispievať či o kategórii, v ktorej je táto téma.",
+	"!post" => "Príspevok je prádzny. Skúste ho vyplniť a potom znova odoslať.",
+	"!ins" => "Príspevok sa nepodarilo vložiť.",
+];
+
+if(!empty($_GET["flash"])) {
+	if(isset($messages[$_GET["flash"]])) {
+		$flashMessage = $messages[$_GET["flash"]];
+		if(in_array($_GET["flash"], [ "!db", "!ins" ]))
+			$flashMessage .= $aMasTo;
+	}
+}
 
 // pre odkomentovanie doctypu jednoducho odstráň sekvenciu -- zo začiatku aj z konca
 ?>
@@ -26,21 +45,26 @@ header("Content-Type: text/html; charset=utf-8", true, 200);
 	
 <div id="forum">
 <div id="content">
+	<a class='button' href='./view_topic.php?<?php echo "tid=$_GET[tid]&cid=$_GET[cid]"; ?>'>Návrat do témy</a>
+	<script>document.write('<a class="button_register" onclick="window.open(&quot;upload.php&quot;, &quot;okno1&quot;, &quot;width=500,height=400&quot;)">Nahrať obrázok</a>')</script>
+	<hr />
 
-	<a class='button' href='javascript:history.back(1)'>Späť</a> <a class='button_register' onclick="window.open('upload.php', 'okno1', 'width=500,height=400')">Nahrať obrázok</a><hr />
-
+	<?php if(!empty($flashMessage)) { ?>
+	<p class="warning"><?php echo $flashMessage ?></p>
+	<?php } else { ?>
 	<form action="post_reply_parse.php" method="post" name="zasli-prispevok">
 		<p>Pridať odpoveď:</p>
 		<textarea name="prispevok" rows="5" cols="75"></textarea><br>
-					<button class='button' id="b" tabindex=0><b>tučné</b></button>
-					<button class='button' id="i" tabindex=0><i>kurzíva</i></button>
-					<button class='button' id="u" tabindex=0><u>podčiarknuté</u></button>
-					<button class='button' id="del" tabindex=0><s>prečiarknuté</s></button>
-		<br /><br />
-		<input type="hidden" name="cid" value="<?php echo $cid; ?>" />
-		<input type="hidden" name="tid" value="<?php echo $tid; ?>" />
+			<button class='button' id="b" tabindex=0><b>tučné</b></button>
+			<button class='button' id="i" tabindex=0><i>kurzíva</i></button>
+			<button class='button' id="u" tabindex=0><u>podčiarknuté</u></button>
+			<button class='button' id="del" tabindex=0><s>prečiarknuté</s></button>
+			<br /><br />
+		<input type="hidden" name="cid" value="<?php echo $_GET['cid']; ?>" />
+		<input type="hidden" name="tid" value="<?php echo $_GET['tid']; ?>" />
 		<input type="submit" name="reply_submit" class="input_button" value="Pridať odpoveď">
 	</form>
+	<?php } ?>
 </div>
 </div>
 </center>
