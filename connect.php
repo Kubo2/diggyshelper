a5719cc1ab91fc02c766f7b3a51d6cf356db3946
Kubo2 <kelerest123@gmail.com>
2014-05-04 02:29:10 +0200

    Rewritten the way connect.php connects to a database.
    connect.php now retrieves data needed for a connection from separated configuration files.
    It is good for a lot of developers when they are working on a project and each of them have his/her own database.

--- connect.php
+++ connect.php
@@ -4,10 +4,21 @@ if(defined('DB_CONNECTED')) return true;
 
 $dbData = array(
 	"host" => 'localhost',
-	"user" => 'skdiggyshelper',
-	"password" => (strpos($_SERVER["SERVER_NAME"], 'localhost') !== false ? NULL : '7FD58A34E5'),
-	"name" => 'skdiggyshelper',
+	"user" => '',
+	"password" => NULL,
+	"name" => 'dh',
 );
+
+// load configuration
+$conf = ".dh.db-config.ini";
+foreach(file("./.dbconfig") as $line) {
+	if(substr($line, 0, 4) == "ref:") {
+		$conf = substr($line, 5);
+		break;
+	}
+}
+
+$dbData = parse_ini_file($conf) + $dbData;
 
 $connection = mysql_connect(
 	$dbData["host"], 
