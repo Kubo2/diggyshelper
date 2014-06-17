e5e010dd17d6e859bad3d2224090d7bbcf182fa4
Kubo2 <kelerest123@gmail.com>
2014-06-17 02:28:11 +0200

    universal.js and selector.js no longer needed
    Because of having one united javascript diggyshelper.js there is no need
    to longer store these separate javascript files.

--- includes/head.php
+++ includes/head.php
@@ -8,5 +8,4 @@ $absUrl = rtrim(dirname($_SERVER["PHP_SELF"]), '/');
 <title><?php echo !empty($titleConst) ? $titleConst : "Diggy's Helper - Prvé česko-slovenksé fórum" ?></title>
 <link rel='stylesheet' href='<?php echo $absUrl; ?>/css/style.css'>
 <link href="<?php echo $absUrl; ?>/favicon.png" rel="icon" type="image/png">
-<!--script type="text/javascript" src="<?php echo $absUrl; ?>/universal.js"></script-->
 <!--script type="text/javascript" src="<?php echo $absUrl; ?>/diggyshelper.js"></script-->
