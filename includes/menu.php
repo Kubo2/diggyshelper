0b0634aa2926b59597d49bbc0866bee5846749f3
Kubo2 <kelerest123@gmail.com>
2014-06-17 02:45:35 +0200

    Update includes/menu.php
    Zakomentované:
    	* zbytočný výber jazyka stránky (chýba totiž preklad)
    	* ul#sutaz
    		1. nemal to byť zoznam, ale skôr <div>
    		2. zatiaľ to nemá cenu, kto bude súťaže ručne vypisovať?

--- includes/menu.php
+++ includes/menu.php
@@ -8,13 +8,14 @@
 	<a href="members.php">Užívateľia</a>&nbsp;&nbsp;&nbsp;
 	<a href="authors.php">Autori</a>
 	
-	<font class="jazykon">Slovenský</font>&nbsp;&nbsp;&nbsp;<a href="">English</a>
+	<!--font class="jazykon">Slovenský</font>&nbsp;&nbsp;&nbsp;<a href="">English</a-->
 	
 </div>
 
-	<ul id="sutaz">
+<!--ul id="sutaz">
 		!!! NOVÁ SÚTAŽ PRÁVE TERAZ !!!<br/><br/>
 		Hrá sa o 50 gemov v hre Diggy's Adventure.<br><br>
 		<a href="contest.php" class="button">Viac informácii</a>
 		
-	</ul>
+	</ul-->
+<!-- nabudúce prosím radšej <div> ~Kubo2 -->
