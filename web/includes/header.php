97b977a2be3008ed0e25ed87af1701aa03089ef9
WladinQ <vladimir.jacko.ml@gmail.com>
2014-09-02 09:17:41 +0200

    uprava userboxu

--- web/includes/header.php
+++ web/includes/header.php
@@ -14,10 +14,17 @@
 			<a class='button_register' href='register.php'>Registrovať sa</a>
 		</form>
 		<?php else: ?>
-		Prihlásený používateľ <strong>"<?php echo($_SESSION['username']) ?>"</strong><br><br>
-		<a class='button' href='./profile.php?user=<?php echo urlencode($_SESSION['username']) ?>'>Môj profil</a>
-		<!--a class='button_register' href='#'>Žiadosti o priateľstvo (0)</a-->
-		<a class='button_logout' href='logout.php'>Odhlásiť sa</a>
+		<img type="userimages" src="http://www.bassboathq.com/wp-content/themes/simply-responsive-cp/images/no-thumb-250.jpg" align="left" alt="Profilová fotografia">
+		Prihlásený používateľ &raquo; <strong><?php echo($_SESSION['username']) ?></strong><br><br>
+		Počet príspevkov: -1<br>
+		Posledná návšteva: 00/00/0000<br>
+		<hr/>
+		<a class='button' title='Môj profil' href='./profile.php?user=<?php echo urlencode($_SESSION['username']) ?>'><img type='userbuttons' src='./images/icon/profile1.png'></a> 
+		<!--a class="button_register" title="Žiadosti o priateľstvo" href="#"><img type="userbuttons" src="icon/friends.png"> (0)</a--> 
+		<!--a class="button_register" title="Upozornenia" href="#"><img type="userbuttons" src="icon/info2.png"> (0)</a-->
+		<!--a class="setting" title="Nastavenia (Administrátor)" href="#"><img type="adminmoderatorbuttons" src="icon/as.png"></a-->
+		<!--a class="setting" title="Nastavenia (Moderátor)" href="#"><img type="adminmoderatorbuttons" src="icon/ms.png"></a-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a style='float:right;' class='button_logout' title='Odhlásiť sa' href='logout.php'><img type='userbuttons' src='./images/icon/off2.png'></a>
 		<?php endif ?>
 	</div>
 </div>
