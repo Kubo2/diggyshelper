91bffed40f71e9bbcf48ede00ea742ac4df74065
Kubo2 <kelerest123@gmail.com>
2014-09-09 18:01:45 +0200

    update userbox's source code

--- web/includes/header.php
+++ web/includes/header.php
@@ -17,12 +17,18 @@
 		Prihlásený používateľ &raquo; <strong><?php echo($_SESSION['username']) ?></strong><br><br>
 		Počet príspevkov: -1<br>
 		Posledná návšteva: 00/00/0000<br>
-		<hr/>
-		<a class='button' title='Môj profil' href='./profile.php?user=<?php echo urlencode($_SESSION['username']) ?>'><img type='userbuttons' src='./images/icon/profile1.png'></a> 
-		<!--a class="button_register" title="Upozornenia" href="#"><img type="userbuttons" src="icon/info2.png"> (0)</a-->
-		<!--a class="setting" title="Nastavenia (Administrátor)" href="#"><img type="adminmoderatorbuttons" src="icon/as.png"></a-->
-		<!--a class="setting" title="Nastavenia (Moderátor)" href="#"><img type="adminmoderatorbuttons" src="icon/ms.png"></a-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		<a style='float:right;' class='button_logout' title='Odhlásiť sa' href='logout.php'><img type='userbuttons' src='./images/icon/off2.png'></a>
+		<hr>
+		<a class='button' title='Môj profil' href='./profile.php?user=<?= urlencode($_SESSION['username']) ?>'>
+			<img type='userbuttons' src='./images/icon/profile1.png'>
+		</a><!--
+			a class="button_register" title="Upozornenia" href="#"><img type="userbuttons" src="icon/info2.png"> (0)</a
+		-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a 
+			style='float: right'
+			class='button_logout'
+			title='Odhlásiť sa'
+			href='logout.php'>
+				<img type='userbuttons' src='./images/icon/off2.png'>
+		</a>
 		<?php endif ?>
 	</div>
 </div>
