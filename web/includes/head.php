093008bc908552dce70bd288fea44e5fc43a60b4
Kubo2 <kubo2@diggyshelper.net>
2015-12-30 01:19:03 +0530

    [TPL] Cosmetics & Consistency
    Changes:
      * use single quotes for strings (readability)
      * use PHP shorthand <?= ?> for echo (readability)
      * @var phpDoc vs line comment description
      * HTML comment vs PHP comment
      * <link> attr order consistency (put [href] before [rel=stylesheet])

--- web/includes/head.php
+++ web/includes/head.php
@@ -1,21 +1,25 @@
 <?php
-// absolute url off current document
-// depends on requested resource's url
-$absUrl = rtrim(dirname($_SERVER["PHP_SELF"]), '/');
+
+/**
+ * absolute url off current document
+ * (depends on requested resource's url)
+ * @var string
+ */
+$absUrl = rtrim(dirname($_SERVER['PHP_SELF']), '/');
 
 // data sanitization library
 require_once __DIR__ . '/../sanitize.lib.php';
 
 ?>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-<!-- Universal title. Only temporarily. -->
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'>
+<?php /* Universal title. Only temporarily. */ ?>
 <title><?=
 			! empty( $titleConst )
 			? SanitizeLib\sanitize($titleConst, SanitizeLib\HTML)
-			: "Diggy's Helper - Prvé česko-slovenksé fórum o hre Diggy's Adventure"
+			: "Diggy's Helper &ndash; Prvé česko-slovenské fórum o hre Diggy's Adventure"
 ?></title>
 
-<link rel='stylesheet' href='<?php echo $absUrl; ?>/css/style.css'>
-<link href="<?php echo $absUrl; ?>/favicon.png" rel="icon" type="image/png">
+<link href='<?= $absUrl ?>/css/style.css' rel='stylesheet'>
+<link href='<?= $absUrl ?>/favicon.png'   rel='icon' type='image/png'>
 
