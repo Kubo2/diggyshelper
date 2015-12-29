4e4757472ee7fda48f96cae04fe7f77f1461081c
Kubo2 <kubo2@diggyshelper.net>
2015-12-30 02:11:30 +0530

    [GA] Simplify the tracking code

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -12,44 +12,6 @@
 	</div>
 </div>
 
+<script>ga('send', 'pageview')</script>
 <script src="<?= @ $absUrl ?: '.' ?>/diggyshelper.js?v=20150812@1.5-alpha2" async></script>
-
-<?php // Google Analytics tracking code ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-</script><script>
-// initialize the tracker
-  ga('create', 'UA-37444056-2', 'auto');
-  ga(function(tracker) {
-  	// set correct title for topics page
-  	if(location.pathname.substr(-15) == '/view_topic.php') {
-  		tracker.set('title', document.getElementById('content').getElementsByTagName('h1')[0].textContent);
-  	}
-
-  	// track clicks to user profiles through topic listing
-  	(function() {
-  		var tbls = document.getElementsByTagName('table');
-  		for(var l = tbls.length, i = 0, topics; i < l; i++) {
-  			if(tbls[i].className.match(/newest-topics/)) {
-  				topics = tbls[i].getElementsByTagName('tbody')[0];
-  				break;
-  			}
-  		}
-  		if(!topics) return;
-
-  		for(var l = topics.rows.length, i = 0; i < l; i++) {
-  			// the link in the second cell of each row
-  			topics.rows[i].cells[1].getElementsByTagName('a')[0].onclick = function() {
-  				ga('send', 'event', 'click', 'frontpage->userprofile', this.textContent.replace(/^\s+|\s+$/g, ''), { transport: 'beacon' })
-  			};
-  		}
-  	})();
-  });
-  
-// send hits
-  ga('send', 'pageview');
-
-</script>
+<script src='http://www.google-analytics.com/analytics.js' async defer></script>
--- web/includes/head.php
+++ web/includes/head.php
@@ -20,6 +20,56 @@ require_once __DIR__ . '/../sanitize.lib.php';
 			: "Diggy's Helper &ndash; Prvé česko-slovenské fórum o hre Diggy's Adventure"
 ?></title>
 
+<?php // {{{ Google Analytics tracking code ============ ?>
+<script>
+
+<?php // shared literals are redistributed already in PHP template
+      // so when any change is needed, you just change the constants
+
+const GA_GLOBAL = 'ga';
+const GA_PROPERTY = 'UA-37444056-2';
+
+?>
+function <?= GA_GLOBAL ?>() { <?= GA_GLOBAL ?>.q.push(arguments) }
+<?= GA_GLOBAL ?>.q = [ ];
+<?= GA_GLOBAL ?>.l = +new Date();
+
+window['GoogleAnalyticsObject'] = <?= json_encode(GA_GLOBAL) ?>;
+
+</script><script>
+// initialize the tracker
+  ga('create', <?= json_encode(GA_PROPERTY) ?>, 'auto');
+
+// correct some not-yet-fixed programming errors
+  ga(function(tracker) {
+  	// set correct title for topics page
+  	if(location.pathname.substr(-15) == '/view_topic.php') {
+  		tracker.set('title', document.getElementById('content').getElementsByTagName('h1')[0].textContent);
+  	}
+
+  	// track clicks to user profiles through topic listing
+  	(function() {
+  		var tbls = document.getElementsByTagName('table');
+  		for(var l = tbls.length, i = 0, topics; i < l; i++) {
+  			if(tbls[i].className.match(/newest-topics/)) {
+  				topics = tbls[i].getElementsByTagName('tbody')[0];
+  				break;
+  			}
+  		}
+  		if(!topics) return;
+
+  		for(var l = topics.rows.length, i = 0; i < l; i++) {
+  			// the link in the second cell of each row
+  			topics.rows[i].cells[1].getElementsByTagName('a')[0].onclick = function() {
+  				ga('send', 'event', 'click', 'frontpage->userprofile', this.textContent.replace(/^\s+|\s+$/g, ''), { transport: 'beacon' })
+  			};
+  		}
+  	})();
+  });
+  
+</script>
+<?php // }}} Google Analytics tracking code ============ ?>
+
 <link href='<?= $absUrl ?>/css/style.css' rel='stylesheet'>
 <link href='<?= $absUrl ?>/favicon.png'   rel='icon' type='image/png'>
 
