31e76e59340757bc575b6b22c698e2318e651086
Kubo2 <kubo2@diggyshelper.net>
2017-03-31 15:17:27 +0200

    Prvoaprílový pokus o žart :-)

--- web/includes/submenu.php
+++ web/includes/submenu.php
@@ -1,1 +1,11 @@
-<div id="provizorne"></div>
+<div id="provizorne">
+	Ale čo, vyzerá to, že tvoj profil zomiera od nudy! Hybaj vytvoriť nejakú tému :-)
+	(<a href='forum.php?1.%20apríl'>vytvoriť tému</a> &middot;
+	<a href='https://google.sk/search?q=1.+apríl'>1. apríl</a>)
+</div>
+<script>(function(p){
+	var qs = window.location.search === '?1.%20apr%C3%ADl';
+	var april, d = new Date();
+	april = d.getMonth() === 3 && d.getDate() === 1;
+	april && !qs && (p.style.display = 'block');
+})(document.getElementById('provizorne'))</script>
